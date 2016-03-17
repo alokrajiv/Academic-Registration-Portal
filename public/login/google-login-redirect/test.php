57e8904889eb4c885904e75ecf66f2c490256ec0
alokrajiv <mail@alokrajiv.com>
2016-03-17 09:48:09 +0400

    Issue: google login azure, patch attempt5

--- public/login/google-login-redirect/test.php
+++ public/login/google-login-redirect/test.php
@@ -57,3 +57,14 @@ class Customizable_Exception extends Exception {
 }
 require_once $_SERVER["DOCUMENT_ROOT"].'/../vendor/google/apiclient/src/Google/autoload.php';
 echo 'Hello';
+
+//Insert your cient ID and secret 
+//You can get it from : https://console.developers.google.com/
+$client_id = '543618368896-pdttlgf1v8caca51dp017npqu1qgcei4.apps.googleusercontent.com'; 
+$client_secret = 'lLkfNni6luZREbSfwx8PBbm_';
+$redirect_uri = 'http://bpdc-arcd.azurewebsites.net/login/google-login-redirect/';
+
+//incase of logout request, just unset the session var
+if (isset($_GET['logout'])) {
+  unset($_SESSION['access_token']);
+}
