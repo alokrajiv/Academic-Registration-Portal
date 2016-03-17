e8c334ecbfca4a5aef548071e6c94c5f526cffa1
alokrajiv <mail@alokrajiv.com>
2016-03-17 09:25:38 +0400

    Issue: google login azure, activating error logging

--- public/login/google-login-redirect/index.php
+++ public/login/google-login-redirect/index.php
@@ -3,18 +3,13 @@ session_start(); //session start
 
 require_once $_SERVER["DOCUMENT_ROOT"].'/../vendor/google/apiclient/src/Google/autoload.php';
 require_once $_SERVER["DOCUMENT_ROOT"] . '/../configs/auto_config.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/../configs/debugger.php';
 
 //Insert your cient ID and secret 
 //You can get it from : https://console.developers.google.com/
 $client_id = '543618368896-pdttlgf1v8caca51dp017npqu1qgcei4.apps.googleusercontent.com'; 
 $client_secret = 'lLkfNni6luZREbSfwx8PBbm_';
-$redirect_uri = getenv("CUSTOMCONNSTR_BASE_URL").'/login/google-login-redirect/';
-
-//database
-$db_username = "xxxxxxxxx"; //Database Username
-$db_password = "xxxxxxxxx"; //Database Password
-$host_name = "localhost"; //Mysql Hostname
-$db_name = 'xxxxxxxxx'; //Database Name
+$redirect_uri = 'http://bpdc-arcd.azurewebsites.net/login/google-login-redirect/';
 
 //incase of logout request, just unset the session var
 if (isset($_GET['logout'])) {
@@ -88,7 +83,4 @@ if (isset($authUrl)){
 	echo '</pre>';
 }
 echo '</div>';
-
-
-?>
 
