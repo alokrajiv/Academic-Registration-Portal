0d907a1cd38dad88fb5a32098cc5a679c49257d1
alokrajiv <mail@alokrajiv.com>
2016-03-17 13:08:42 +0400

    Google Login Woring on Azure

--- configs/set_env_variables.php
+++ configs/set_env_variables.php
@@ -1,6 +1,6 @@
 <?php
 
-if(!getenv("PROD")){
+if(!getenv("CUSTOMCONNSTR_PROD")){
     //loading dev_env.php if not production
     require_once __DIR__."/dev_env.php";
     putenv("BASE_DIR=".__DIR__."/../");
--- public/login/google-login-redirect/index.php
+++ public/login/google-login-redirect/index.php
@@ -3,7 +3,7 @@ session_start(); //session start
 
 require_once $_SERVER["DOCUMENT_ROOT"].'/../vendor/google/apiclient/src/Google/autoload.php';
 //require_once $_SERVER["DOCUMENT_ROOT"] . '/../configs/auto_config.php';
-require_once $_SERVER["DOCUMENT_ROOT"] . '/../configs/debugger.php';
+//require_once $_SERVER["DOCUMENT_ROOT"] . '/../configs/debugger.php';
 
 //Insert your cient ID and secret 
 //You can get it from : https://console.developers.google.com/
