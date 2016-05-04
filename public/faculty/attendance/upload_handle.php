2725056b95fba9fc2a5014845fe6a0372974640e
alokrajiv <mail@alokrajiv.com>
2016-05-04 17:08:13 +0400

    Attendance data parsed

--- public/faculty/attendance/upload_handle.php
+++ public/faculty/attendance/upload_handle.php
@@ -31,17 +31,40 @@ if ($uploadOk == 0) {
     if (move_uploaded_file($_FILES["xlsx_file"]["tmp_name"], $target_file)) {
         echo "The file " . basename($_FILES["xlsx_file"]["name"]) . " has been uploaded.";
 
-        $Reader = new SpreadsheetReader($target_file);
-        $sheets = $Reader->Sheets();
-        echo "<br>" . count($sheets) . " sheets founds. The first will be taken automatically.";
-        echo "<br>The entire faculty table is going to be flushed and loaded with this data.";
-        
+        $inputFileName = $target_file;
+
+//  Read your Excel workbook
+        try {
+            $inputFileType = PHPExcel_IOFactory::identify($inputFileName);
+            $objReader = PHPExcel_IOFactory::createReader($inputFileType);
+            $objPHPExcel = $objReader->load($inputFileName);
+        } catch (Exception $e) {
+            die('Error loading file "' . pathinfo($inputFileName, PATHINFO_BASENAME) . '": ' . $e->getMessage());
+        }
+        $to_save_data = array();
+//  Get worksheet dimensions
+        $sheet = $objPHPExcel->getSheet(0);
+        $highestRow = $sheet->getHighestRow();
+        $highestColumn = $sheet->getHighestColumn();
+        $controlRowData = $sheet->rangeToArray('A' . 4 . ':' . $highestColumn . 4, NULL, TRUE, FALSE);
+        echo("<br>{$controlRowData[0][2]}------{$controlRowData[0][5]}------{$controlRowData[0][7]}");
+        array_push($to_save_data, array($controlRowData[0][2], $controlRowData[0][5], $controlRowData[0][7]));
+//  Loop through each row of the worksheet in turn
+        for ($row = 12; $row <= $highestRow; $row++) {
+            //  Read a row of data into an array
+            $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE);
+            //var_dump($rowData);
+            echo "<br>{$rowData[0][1]}------------{$rowData[0][10]}";
+            array_push($to_save_data, array($rowData[0][1], $rowData[0][10]));
+        }
+        var_dump($to_save_data);
+        $_SESSION['attendance_data_to_process'] = $to_save_data;
+        die("<h1>MANUAL FORCE DIE TO PAGE: Finished iterating thru the sheet and saving required data to current session!!</h1>");
         echo '<a href="sheet_to_db.php?filename=' . basename($target_file) . '">CONFIRM</a>';
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
 }
-
 
 function uniqid_base36($more_entropy = true) {
     $s = uniqid('', $more_entropy);
