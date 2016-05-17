f0e54335f669c85154754e202cfc42b2db120119
alokrajiv <mail@alokrajiv.com>
2016-05-18 03:55:30 +0400

    Rewire of database schema and corresponding changes in application

--- public/student/attendance/index.php
+++ public/student/attendance/index.php
@@ -2,19 +2,22 @@
 session_start();
 require_once $_SERVER["DOCUMENT_ROOT"] . '/../configs/auto_config.php';
 $sql1 = "SELECT "
-        . "`faculty_list`.`full_name`, `faculty_course_list`.`psrn_no`, "
-        . "`course_list`.`course_name`, `student_course_list`.`comp_code`, "
-        . "`student_course_list`.`section_code`, "
+        . "`faculty_list`.`full_name` AS 'faculty_name', "
+        . "`faculty_course_list`.`psrn_no`, "
+        . "`course_list`.`course_name`, "
+        . "`student_course_attendance_list`.`comp_code`, "
+        . "`student_course_attendance_list`.`section_code`, "
         . "`student_course_attendance_list`.`attendance` "
         . "FROM "
-        . "`student_course_list` JOIN `student_course_attendance_list` JOIN "
-        . "`course_list` JOIN `faculty_course_list` JOIN `faculty_list` "
-        . "ON "
-        . "`student_course_list`.`id` = `student_course_attendance_list`.`id` AND"
-        . " `student_course_list`.`comp_code` = `course_list`.`comp_code` AND"
-        . " `faculty_course_list`.`comp_code` = `student_course_list`.`comp_code` AND"
-        . " `faculty_course_list`.`section_code` = `student_course_list`.`section_code` AND"
-        . " `faculty_list`.`psrn_no` = `faculty_course_list`.`psrn_no`"
+        . "`student_course_attendance_list` JOIN "
+        . "`course_list` JOIN "
+        . "`faculty_course_list` JOIN "
+        . "`faculty_list` "
+        . "ON"
+        . " `student_course_attendance_list`.`comp_code` = `course_list`.`comp_code` AND"
+        . " `faculty_course_list`.`comp_code` = `student_course_attendance_list`.`comp_code` AND "
+        . "`faculty_course_list`.`section_code` = `student_course_attendance_list`.`section_code` AND"
+        . " `faculty_course_list`.`psrn_no` = `faculty_list`.`psrn_no`"
         . "WHERE "
         . "`bits_id` = ?";
 try {
@@ -79,7 +82,7 @@ and open the template in the editor.
                                     echo "<tr onclick=''>
                                             <td>{$value['course_name']}</td>
                                             <td>{$value['section_code']}</td>
-                                            <td>{$value['full_name']}</td>
+                                            <td>{$value['faculty_name']}</td>
                                             <td>{$value['attendance']} %</td>
                                             <td><button class='btn btn-default' ><a href='https://www.google.com' target='_blank'>Click Here</a></button> </td>
                                         </tr>";
