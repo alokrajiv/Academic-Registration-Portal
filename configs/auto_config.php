df0abb628d2934e728089ff468c9e5e6ec41d0f5
alokrajiv <mail@alokrajiv.com>
2016-09-11 22:41:04 +0400

    Wrote function to secure pages

--- configs/auto_config.php
+++ configs/auto_config.php
@@ -1,6 +1,24 @@
 <?php
 
-require_once __DIR__."/db_config.php";
+require_once __DIR__ . "/db_config.php";
 //require_once __DIR__."/debugger.php";
-require_once __DIR__."/set_env_variables.php";
+require_once __DIR__ . "/set_env_variables.php";
 require_once __DIR__ . "/../vendor/autoload.php";
+
+function checkIfLoggedInAs($type) {
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+    if ($_SESSION['user_data']['type'] !== $type) {
+        safeRedirect("/");
+    }
+}
+
+function safeRedirect($new_url) {
+    if (!headers_sent()) {
+        header("Location: $new_url");
+    } else {
+        echo "<script>window.location.href = '$new_url';</script>";
+    }
+    exit();
+}
